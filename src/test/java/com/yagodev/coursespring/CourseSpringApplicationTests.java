90b89e68a96a5306c45af0dc06d6cc32c4ec9d50
Yago Garcez <yago.garcez@outlook.com>
2022-09-03 22:57:41 -0300

    Project created.

---
+++ src/test/java/com/yagodev/coursespring/CourseSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yagodev.coursespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
